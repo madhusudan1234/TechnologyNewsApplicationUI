803568a0ac2bf7f1638ec8705445204fcf4bd72b
Madhusudan Vratesh <madhusudan.education7@gmail.com>
2022-10-31 11:57:55 +0530

    intial commit

---
+++ TechnologyNewsApplicationUI/Models/ErrorViewModel.fs
@@ -0,0 +1,9 @@
+namespace TechnologyNewsApplicationUI.Models
+
+open System
+
+type ErrorViewModel =
+    { RequestId: string }
+
+    member this.ShowRequestId =
+        not (String.IsNullOrEmpty(this.RequestId))
